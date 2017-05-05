ffdf2733df8fdadd9f0d436607dda06187979101
Quoc Anh <quocanh1897@gmail.com>
2017-05-06 01:46:17 +0700

    update log in to dev2

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
 int main() {
-
-	cin.get();
-	cin.get();
-	return 0;
+	string str, username, pass;
+	cout << "----------------------------------------------------------------" << endl
+		<< "----------------------------LOG IN------------------------------" << endl
+		<< "----------------------------------------------------------------" << endl << endl << endl;
+LOGIN:
+	cout << "USER NAME: ";
+	cin >> username;
+	cout << "PASSWORD:  ";
+	cin >> pass;
+	fstream f;
+	f.open("account.txt");
+	while (!f.eof()) {
+		stringstream ss;
+		getline(f, str);
+		ss << str;
+		string u, p;
+		ss >> u >> p;
+		if (u == username&&p == pass) goto SUCCESS;
+		ss.clear();
+	}
+	cout << "Wrong username or password," << endl;
+	goto LOGIN;
+SUCCESS:
+	cout << "Log in successfully" << endl;
+	f.close();
+	cout << "Press R to (R)eturn to the previous page" << endl
+		<< "Press S to go to (S)tart page" << endl;
+	char choice;
+	cin >> choice;
 }
