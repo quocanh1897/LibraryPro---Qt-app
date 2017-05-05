7ad142c40d76452e2b9031011aa93ae8dfd5c489
Quoc Anh <quocanh1897@gmail.com>
2017-05-06 01:10:27 +0700

    tính năng log-in

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
 int main() {
+	string str, username, pass;
+	cout << "----------------------------------------------------------------" << endl
+		<< "----------------------------LOG IN------------------------------" << endl
+		<< "----------------------------------------------------------------" << endl << endl << endl;
+	cout << "USER NAME: ";
+	cin >> username;
+	cout << "PASSWORD: ";
+	cin >> pass;
+	fstream f;
+	f.open("account.txt");
+	while (!f.eof()) {
+		stringstream ss;
+		getline(f, str);
+		ss << str;
 
+	}
+	return 0;
 }
