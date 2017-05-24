2515e43ae259091b4ae1d58f84ee1224882552ee
Quoc Anh <quocanh1897@gmail.com>
2017-05-24 12:51:43 +0700

    no message

--- Qt GUI/LIBPRO/signin.cpp
+++ Qt GUI/LIBPRO/signin.cpp
@@ -7,7 +7,7 @@ signin::signin(QWidget *parent) :
 {
     ui->setupUi(this);
     QSqlDatabase mydb=QSqlDatabase::addDatabase("QSQLITE");
-    mydb.setDatabaseName("D:/sqlite/book.sqlite");
+    mydb.setDatabaseName("C:/Users/You/Documents/Assignment2_cpp/Qt GUI/LIBPRO/book.sqlite");
     if (!mydb.open())
        {
           qDebug() << "Error: connection with database fail";
@@ -31,17 +31,14 @@ void signin::on_pushButton_clicked()
     accountname=ui->lineEdit_accountname->text();
     password=ui->lineEdit_password->text();
     QSqlQuery qry;
-    /*qry.prepare("select * from account where accountname = :accountname;");
-    qry.bindValue(":accountname",accountname);
-    if(!qry.exec()) QMessageBox::about(this,"Đăng nhậpfail!",accountname);*/
-
-   if(qry.exec("select * from account where accountname='"+accountname+"' and password='"+password+"' ;")) {
-       if(qry.next())
-       QMessageBox::about(this,"Thành công!","Chào mừng bạn đến với LIBPRO");
-       else
-          QMessageBox::warning(this,"Đăng nhập thất bại!","Tài khoản hoặc mật khẩu không đúng!");
-    }
-
-
+        qry.prepare("select * from account where accountname = :accountname and password = :password and reader = :reader;");
+        qry.bindValue(":accountname",accountname);qry.bindValue(":password",password);
+        if(qry.exec()){
+           if(qry.next())
+                QString reader = qry.value(2).toBool();
+                QMessageBox::about(this,"Thành công!","Chào mừng bạn đến với LIBPRO");
+           else
+            QMessageBox::warning(this,"Đăng nhập thất bại!","Tài khoản hoặc mật khẩu không đúng!");
+        }
 }
 
